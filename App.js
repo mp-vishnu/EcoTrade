789ca8bcec76674cf469bcbff284770a86916846
mp-vishnu <mpvishnu12@gmail.com>
2024-06-20 19:31:28 +0530

    added AppInputText component

--- App.js
+++ App.js
@@ -2,43 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image,SafeAreaView, TouchableWithoutFeedback, Button, Alert, Dimensions } from 'react-native';
 import One from './one';
 import Two from './two'
-import ViewImageScreen from './app/components/Screen/ViewImageScreen';
-import Welcome from './app/components/Screen/Welcome';
-import Appbtn from './app/components/button/Appbtn';
-import Card from './app/components/Card/Card';
-import ListItem from './app/components/ListItem';
-import ListingDetailsScreen from './app/components/Screen/ListingDetailsScreen';
-import MessageScreen from './app/components/Screen/MessageScreen';
-import AccountScreen from './app/components/Screen/AccountScreen';
-import Icon from './app/components/Icon';
-import ListingsScreen from './app/components/Screen/ListingsScreen';
+import AppTextInput from './app/components/AppTextInput';
 import { useDimensions,useDeviceOrientation } from '@react-native-community/hooks';
 export default function App() {
 console.log(useDeviceOrientation());
   return (
- //   <Welcome/>
-//  <ViewImageScreen/>
-//   <Appbtn title="LOGIN" onPress={()=>console.log("hic")}/> 
+
 <View style={styles.container}>
-{/* <Card
- title="Red jacket"
- subTitle="$100"
- image={require("./app/assets/jacket.jpg")}/> */}
- {/* <ListItem
- title="Red jacket"
- subTitle="$100"
- image={require("./app/assets/jacket.jpg")}/> */}
- {/* <ListingDetailsScreen/> */}
- {/* <Icon
- name="email"
- size={50}
- backgroundColor="red"
- iconColor="white" /> */}
- {/* <ListItem title="My title" 
- subTitle={" "}
- IconComponent={<Icon name='email'/>}/> */}
-{/* <AccountScreen/> */}
-<ListingsScreen/>
+<AppTextInput placeholder="Username" icon="email"/>
 </View>
 
   );
